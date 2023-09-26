96b3c44c27f6b1b2e9739255621aad860ba72fa2
Mani <manesorapalli@gmail.com>
2023-09-26 21:17:03 +0530

    update

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -23,7 +23,7 @@ resource "null_resource" "frontend" {
 cd /home/centos/infra-ansible
 git pull
 sleep 60
-ansible-playbook -i ${aws_instance.frontend.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=fronted
+ansible-playbook -i ${aws_instance.frontend.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=frontend
 EOF
   }
 }
